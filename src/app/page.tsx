35d96cadafe8a7a6927b7cba2311d922cae1ccbe
Michael Zaleski <me@mikezaleski.com>
2025-02-07 12:42:55 -0500

    change text in page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,9 @@ export default function Home() {
             </code>
             .
           </li>
-          <li>Save and see your changes instantly.</li>
+          <li>
+            Save and see your changes instantly — like <em>really quickly</em>.
+          </li>
         </ol>
 
         <div className="flex gap-4 items-center flex-col sm:flex-row">
