bdde8276ce3656f9425bf6bcc10dc4f1bf472ab1
Michael Zaleski <me@mikezaleski.com>
2025-02-10 13:33:41 -0500

    Database - Seeding the Database - Part 2

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -4,47 +4,34 @@ const prisma = new PrismaClient();
 
 const tickets = [
   {
-    id: "1",
     title: "Ticket 1",
     content: "First ticket from DB.",
     status: "DONE" as const,
   },
   {
-    id: "2",
     title: "Ticket 2",
     content: "Second ticket from DB.",
     status: "OPEN" as const,
   },
   {
-    id: "3",
     title: "Ticket 3",
     content: "Third ticket from DB.",
     status: "IN_PROGRESS" as const,
   },
 ];
 
-// const seed = async () => {
-//   for (const ticket of tickets) {
-//     await prisma.ticket.create({
-//       data: ticket,
-//     });
-//   }
-// };
-
-// const seed = async () => {
-//   const promises = tickets.map((ticket) => {
-//     return prisma.ticket.create({
-//       data: ticket,
-//     });
-//   });
-
-//   await Promise.all(promises);
-// };
-
 const seed = async () => {
+  const t0 = performance.now();
+  console.log("DB Seed: Started...");
+
+  await prisma.ticket.deleteMany();
+
   await prisma.ticket.createMany({
     data: tickets,
   });
+
+  const t1 = performance.now();
+  console.log(`DB Seed: Finished (${t1 - t0}ms)`);
 };
 
 seed();
