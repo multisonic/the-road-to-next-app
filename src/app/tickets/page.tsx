f99ecf354328871429d90b85f7aac6475f550ebb
Michael Zaleski <me@mikezaleski.com>
2025-02-10 11:50:56 -0500

    Fallback Routes - Error Route - Use Placeholder Component

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -4,6 +4,7 @@ import { TicketList } from "@/features/ticket/components/ticket-list";
 import { Spinner } from "@/components/spinner";
 
 const TicketsPage = () => {
+  throw new Error("This is a component error");
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
