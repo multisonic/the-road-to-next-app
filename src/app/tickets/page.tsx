7826836013abb503c55e58798ff12795fabbb405
Michael Zaleski <me@mikezaleski.com>
2025-02-10 11:38:20 -0500

    Server Components - Suspense (Fallback)

--- src/app/tickets/page.tsx
+++ src/app/tickets/page.tsx
@@ -1,13 +1,14 @@
 import Heading from "@/components/heading";
 import { Suspense } from "react";
 import { TicketList } from "@/features/ticket/components/ticket-list";
+import { Spinner } from "@/components/spinner";
 
 const TicketsPage = () => {
   return (
     <div className="flex-1 flex flex-col gap-y-8">
       <Heading title="Tickets" description="All your tickets in one place" />
 
-      <Suspense>
+      <Suspense fallback={<Spinner />}>
         <TicketList />
       </Suspense>
     </div>
