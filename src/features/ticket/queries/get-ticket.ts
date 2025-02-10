3634aad741d140afd6ea545a92afe098e5d2ecf9
Michael Zaleski <me@mikezaleski.com>
2025-02-10 12:06:58 -0500

    Fallback Routes - Nearest Boundary Bubbling

--- src/features/ticket/queries/get-ticket.ts
+++ src/features/ticket/queries/get-ticket.ts
@@ -4,6 +4,8 @@ import { Ticket } from "../types";
 export const getTicket = async (id: string): Promise<Ticket | null> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  throw new Error("Oops again!");
+
   const maybeTicket = initialTickets.find((ticket) => ticket.id === id);
 
   return new Promise((resolve) => {
