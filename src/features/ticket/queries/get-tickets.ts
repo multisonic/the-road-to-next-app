2ee4cd49502c036568b694e903992740a46309c0
Michael Zaleski <me@mikezaleski.com>
2025-02-10 11:57:25 -0500

    Fallback Routes - Error Boundary (Optional)

--- src/features/ticket/queries/get-tickets.ts
+++ src/features/ticket/queries/get-tickets.ts
@@ -4,6 +4,7 @@ import { Ticket } from "../types";
 export const getTickets = async (): Promise<Ticket[]> => {
   await new Promise((resolve) => setTimeout(resolve, 2000));
 
+  throw new Error("oh gawd no!");
   return new Promise((resolve) => {
     resolve(initialTickets);
   });
