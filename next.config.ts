1f9d63aa6f48abec422f010b314f6877cf5da483
Michael Zaleski <me@mikezaleski.com>
2025-02-11 11:03:27 -0500

    Caching - Router Cache

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,12 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  /* Router Cache - unintended consequences */
+  // experimental: {
+  //   staleTimes: {
+  //     dynamic: 30,
+  //   },
+  // },
 };
 
 export default nextConfig;
